32d406f7ebced685f567f68d570ace3031ca251a
Gleison Amorim <54336609+GleisonAmorim@users.noreply.github.com>
2025-04-08 13:29:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -297,4 +297,4 @@ def logout():
     return redirect('/login')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
